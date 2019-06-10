dd11c2e95c82b7296376b275d433c828b187526c
James McDowell <wejrox@users.noreply.github.com>
2019-06-10 19:23:56 +1000

    Updated test for mobile toggle
    Toggle should only be run on mobile

--- src/tests/core/navigation/NavigationBar.test.tsx
+++ src/tests/core/navigation/NavigationBar.test.tsx
@@ -1,19 +1,23 @@
 import * as Enzyme from "enzyme";
 import * as React from "react";
 import NavigationBar from "../../../main/core/navigation/NavigationBar";
+import * as sinon from "sinon";
 
 describe("NavigationBar Component", () => {
+    const toggleSpy = sinon.spy();
+    sinon.replace(NavigationBar.prototype, "toggle", toggleSpy);
     const wrapper = Enzyme.shallow(<NavigationBar/>);
 
     it("renders correctly", () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("toggles open state", () => {
+    it("does not toggle open state if not on mobile", () => {
         const prvOpenState: boolean = (wrapper.instance() as NavigationBar).state.isOpen;
         (wrapper.instance() as NavigationBar).toggle();
 
-        expect((wrapper.instance() as NavigationBar).state.isOpen).toBe(!prvOpenState);
+        expect(toggleSpy.called).toBe(true);
+        expect((wrapper.instance() as NavigationBar).state.isOpen).toBe(prvOpenState);
     });
 
 });
