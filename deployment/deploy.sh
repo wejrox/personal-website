6ec046d443144001c1dca9bc73b3ce4f084ad393
James McDowell <wejrox@users.noreply.github.com>
2020-11-24 20:50:50 +1100

    Attemped to reduce build time by combining stages and caches
    Each stage was running npm install, and using credits to do so. This will hopefully reduce that requirement.

---
+++ deployment/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+IMAGE_NAME="$DOCKER_USERNAME/$DOCKER_IMAGE_NAME"
+
+docker build . -t $IMAGE_NAME:$TRAVIS_BUILD_NUMBER
+
+if [ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker tag $IMAGE_NAME:$TRAVIS_BUILD_NUMBER $IMAGE_NAME:latest
+  docker push $IMAGE_NAME:latest
+fi
